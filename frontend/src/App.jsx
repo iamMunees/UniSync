308d12f5155c836b4456995002a7b333c5641dcc
iamMunees <dev.iammunees@gmail.com>
2025-04-26 19:34:15 +0530

    changing the db password and email and some changes on mailtrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,6 +35,8 @@ function App() {
 				<Route path='/' element={authUser ? <HomePage /> : <Navigate to={"/login"} />} />
 				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
 				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+				<Route path='/notifications' element={authUser ? <NotificationsPage /> : <Navigate to={"/login"} />} />
+				
       </Routes>
       <Toaster/>
     </Layout>
