12a061f6f856d7f17f99fe6a3f329b745a50704b
iamMunees <dev.iammunees@gmail.com>
2025-04-29 11:15:47 +0530

    about,skill,education section developed

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
 origin:"http://localhost:5173",
-credentials:"true",
+credentials:true,
 }));
 app.use(express.json({limit:"5mb"}));   //json request
 app.use(cookieParser());
